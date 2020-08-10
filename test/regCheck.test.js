5fffa9fdea322244dfb8c64364f256f57662dbc0
Loreen-netizen <loreenmuusha@gmail.com>
2020-08-10 16:15:24 +0200

    first bootcamp test

---
+++ test/regCheck.test.js
@@ -0,0 +1,12 @@
+let assert = require("assert");
+let regCheck = require("regCheck");
+
+describe("regCheck", function(){
+    it("should return true for a regNumber from Gauteng",function(){
+        assert.equal(true,regCheck('DV 23 NB GP', 'GP'))
+    })
+    it("should return false for a regNumber matched with wrong town",function(){
+        assert.equal(false,regCheck('DV 23 LP GP', 'MP'))
+    })
+    
+    });
