c2dce9ad6031647b4ac47af7baa6541de86c26f6
Loreen-netizen <loreenmuusha@gmail.com>
2020-08-11 07:08:46 +0200

    final bootcamp test

--- findItemsOver20.js
+++ findItemsOver20.js
@@ -10,22 +10,6 @@ var findItemsOver20 = function (objectListArray) {
     return listItemsOver20;
 };
 
-
-
-
-var findItemsOver = function (objectListArray, threshold) {
-    var listItemsOverThreshold = [];
-    for (let i = 0; i < objectListArray.length; i++) {
-        var listItemName = objectListArray[i];
-        var listItemQuantity = objectListArray[i].qty;
-        if (listItemQuantity > threshold) {
-            listItemsOverThreshold.push(listItemName)
-        };
-        console.log(listItemsOverThreshold);
-    }
-    return listItemsOverThreshold;
-};
-
 module.exports = findItemsOver20;
 
 
